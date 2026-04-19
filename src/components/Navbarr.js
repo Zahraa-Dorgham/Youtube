4c6ef90158552336a1be9650a61895abaabeb00d
Zahraa-Dorgham <zahraaadorgham7704@gmail.com>
2026-04-19 20:55:38 +0100

    fix navbar

--- src/components/Navbarr.js
+++ src/components/Navbarr.js
@@ -1,83 +1,56 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 function Navbarr() {
-    const [searchQuery, setSearchQuery] = useState("");
-
-    const handleSearch = (e) => {
-        e.preventDefault();
-        console.log("Recherche pour:", searchQuery);
-    };
-
-    const handleCreate = () => {
-        console.log("Créer un nouveau contenu");
-    };
-
-    const handleLogoClick = () => {
-        console.log("Redirection vers l'accueil");
-    };
-
-    const handleNotifications = () => {
-        console.log("Notifications");
-    };
-
-    const handleProfile = () => {
-        console.log("Profil utilisateur");
-    };
-
     return (
         <div className='mynav'>
-            {/* Logo */}
-            <div className='nav-logo' onClick={handleLogoClick}>
-                <img src='https://logos-marques.com/wp-content/uploads/2021/03/Youtube-Logo.png'
-                    alt="YouTube Logo" />
+            
+            <button className="menu">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                    <path d="M3 6h18v2H3V6zm0 5h18v2H3v-2zm0 5h18v2H3v-2z" fill="currentColor" />
+                </svg>
+            </button>
+
+           
+            <div className='nav-logo'>
+                <img src='https://logos-marques.com/wp-content/uploads/2021/03/Youtube-Logo.png' alt="YouTube Logo" />
             </div>
 
-            {/* Search Bar */}
-            <form className='search' onSubmit={handleSearch}>
-                <input 
-                    type="text" 
-                    placeholder="Rechercher..." 
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                />
-                <button className='search-btn' type="submit">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="20" height="20">
-                        <path d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z" fill="currentColor" />
-                    </svg>
+            <div className='search-container'>
+                <form className='search'>
+                    <input type="text" placeholder="Rechercher" />
+                    <button className='search-btn' type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="20" height="20">
+                            <path d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z" fill="currentColor" />
+                        </svg>
+                    </button>
+                </form>
+                
+                
+                <button className='mic-btn'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                    <path d="M320 64C267 64 224 107 224 160L224 288C224 341 267 384 320 384C373 384 416 341 416 288L416 160C416 107 373 64 320 64zM176 248C176 234.7 165.3 224 152 224C138.7 224 128 234.7 128 248L128 288C128 385.9 201.3 466.7 296 478.5L296 528L248 528C234.7 528 224 538.7 224 552C224 565.3 234.7 576 248 576L392 576C405.3 576 416 565.3 416 552C416 538.7 405.3 528 392 528L344 528L344 478.5C438.7 466.7 512 385.9 512 288L512 248C512 234.7 501.3 224 488 224C474.7 224 464 234.7 464 248L464 288C464 367.5 399.5 432 320 432C240.5 432 176 367.5 176 288L176 248z" /></svg>
                 </button>
-            </form>
-
-            
+            </div>
+ 
             <div className='nav-icons'>
                
-                <button className='nav-btn create-btn' onClick={handleCreate}>
+                <button className='create-btn'>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z" fill="currentColor" />
+                        <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" fill="currentColor" />
                     </svg>
                     <span>Créer</span>
                 </button>
 
-                
-                <button className='nav-icon-btn' onClick={handleNotifications} title="Notifications">
+               
+                <button className='nav-icon-btn'>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
                         <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z" fill="currentColor" />
                     </svg>
                 </button>
 
                 
-                <button className='nav-icon-btn' title="Applications">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                        <path d="M6 8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm12 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" fill="currentColor" />
-                    </svg>
-                </button>
-
-                
-                <button className='nav-icon-btn profile-btn' onClick={handleProfile} title="Profil">
-                    <img 
-                        className='user-avatar' 
-                        src='https://yt3.ggpht.com/ytc/AAUvwnjZ3v5T5RFz7Vl7P3Y5XZ_OZz-FNHXG9Ef-=s88-c-k-no-rj-mo' 
-                        alt="Profil" 
-                    />
+                <button className='profile-btn' >
+                    <img className='user-avatar' src='https://www.hawaiimagazine.com/wp-content/uploads/2020/12/plumeria-2-Eric-Tessmer-Flickr-1024x708.jpg' alt="Profil" />
                 </button>
             </div>
         </div>
